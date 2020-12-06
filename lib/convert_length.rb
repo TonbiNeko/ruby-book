ad7659477cd1167b18b599c983272bc1897eab6a
TonbiNeko <junkomikami312nz@gmail.com>
2020-12-06 18:20:01 +0900

    add coment

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -4,26 +4,31 @@ def convert_length(length, from: :m, to: :m)
 end
 
 
-m: 1.0, ft: 3.28, in: 39.37
-変換後の長さを求める式は
-変換前の単位の長さ/変換前の単位の比率*変換後の単位の比率
+# m: 1.0, ft: 3.28, in: 39.37
+# 変換後の長さを求める式は
+# 変換前の単位の長さ/変換前の単位の比率*変換後の単位の比率
 
-１メートルをインチに直すのは
-1m / 1.0 * 39.37 = 39.37
-１フィートを１メートルは
-1ft / 3.28 * 1.0 = 0.30 
+# １メートルをインチに直すのは
+# 1m / 1.0 * 39.37 = 39.37
+# １フィートを１メートルは
+# 1ft / 3.28 * 1.0 = 0.30 
 
-.round(2)で小数点第三位で四捨五入している
+# .round(2)で小数点第三位で四捨五入している
 
-１
-def convert_length(length, unit_from, unit_to)
-  39.37
-end
-convert_lengthは39.37を返す
+# １
+# def convert_length(length, unit_from, unit_to)
+#   39.37
+# end
+# convert_lengthは39.37を返す
 
-２
-def convert_length(length, unit_from, unit_to)
-  units = { m: 1.0, ft: 3.28, in: 39.37 }
-  (length / units[unit_from] * units[unit_to]).round(2)
-end
+# ２
+# def convert_length(length, unit_from, unit_to)
+#   units = { m: 1.0, ft: 3.28, in: 39.37 }
+#   (length / units[unit_from] * units[unit_to]).round(2)
+# end
+# units[unit_from] 、units[unit_to]で、ハッシュから値を取得している
 
+# def convert_length(length, from: :m, to: :m)
+# キーワード引数を使ってる
+# デフォルト値はどちらもシンボルのメートル
+# ・・・なんのメリットがあるのだろうか？
